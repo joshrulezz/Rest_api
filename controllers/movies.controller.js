de7661059ae45339c17cdc0e8ed61940f87d68cb
joshrulezz <matjoshrix@gmail.com>
2024-06-24 23:49:21 +0530

    over

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,11 +1,17 @@
 import Movie from "../models/movie.model.js";
 
-export const movieIndex = (req , res) => {
-    res.send("get all the movies !");
+export const movieIndex = async(req , res) => {
+    try {
+        const movies = await Movie.find();
+        res.json(movies);
+    }
+    catch(error){
+        res.status(500);
+    }
 }
 export const movieCreate = async(req , res) => {
      //Creating new data and adding in the type of mongo db schema model type 
-     
+
     const newMovie = new Movie({
         id : req.body.id , 
         name : req.body.name , 
@@ -23,11 +29,46 @@ export const movieCreate = async(req , res) => {
     console.log(req.body);
     res.json(req.body);
 }
-
-export const movieUpdate = (req , res) => {
-    res.send("Movie has been updated ");
+export const movieSearch = async(req , res) => {
+    try{
+    const movie = await Movie.findById(req.params.id); // Searching the respective id of the movie..
+    if(movie == null){
+        res.status(500);
+    }
+    else{
+        res.json(movie);
+     }
+    }
+    catch(error){
+        res.status(401);
+    }
+}
+export const movieUpdate = async (req , res) => {
+    try{
+    const result = await Movie.findOneAndUpdate(
+        {_id : req.params.id} , 
+        {
+            name : req.body.title ,     
+            friend : req.body.friend,
+        },
+    );
+    res.status(200).json(result);
+}
+catch(error){
+    res.status(500);
+}
 }
 
-export const movieDelete = (req , res) => {
-    res.send("Movie has been deleted ");
+export const movieDelete = async (req , res) => {
+    const movieId = req.params.id ; 
+
+
+    try{
+        await Movie.deleteOne({_id : movieId});
+        res.json({message : "movie deleted !"});
+    }
+    catch(error){
+        res.status(400);
+
+    }
 }
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { movieIndex , movieCreate , movieDelete , movieUpdate} from "../controllers/movies.controller.js";
+import { movieIndex , movieCreate , movieDelete , movieUpdate , movieSearch} from "../controllers/movies.controller.js";
 const router = express.Router();
 
 
 //CRUD FOR MOVIES 
 
 router.get("/" , movieIndex); // routing the incoming request to controller from this file !
+
+router.get("/:id" , movieSearch);
 
 router.post("/" , movieCreate);
 
